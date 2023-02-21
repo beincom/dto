db157477d287092a1d7eee6d89181f5932dfe1da
Nguyen Minh Dat <minhdat@tgm.vn>
2023-02-21 18:36:44 +0700

    BEIN-12392 update activity log DTO

--- src/activity-log.dto.ts
+++ src/activity-log.dto.ts
@@ -18,17 +18,18 @@ export enum OBJECT_TYPES {
 
 export class Actor {
   id: string;
-  avatarUrl: string;
+  username: string;
   fullname: string;
+  avatarUrl: string;
 }
 
 export class ChangeSummary {
-  [propName: string]: { old: string | Object; new: string | Object };
+  [propName: string]: { old: string | object; new: string | object };
 }
 
 /**
  * The DTO contract for activity log item.
- * 
+ *
  * This object will be used by BIC-services to write activity log info to the log queue (KAFKA),
  * which will be consumed by activity-log-service afterwards
  */
@@ -38,29 +39,30 @@ export class ActivityLogItem {
   eventType: EVENT_TYPES;
   objectType: OBJECT_TYPES;
   objectId: string;
-  
+
   /** The original states of the object BEFORE making changes
-   * 
+   *
    *  Use for detect change logs when comparing to `{currentState}`
-   * 
+   *
    *  Can be `null` in cases of `CREATE`
    */
-  originalState: Object;
-  
+  originalState: object;
+
   /** The newest states of the object AFTER making changes
-   * 
+   *
    *  Use for detect change logs when comparing to `{originalState}`
-   * 
+   *
    *  Can be `null` in cases of `DELETE`
    */
-  currentState: Object;
+  currentState: object;
 
-  /** 
+  /**
    * OPTIONAL: The detail changes of the activity log event
-   * 
+   *
    * Only use this when producer-service cannot share the full states of the changed entity (`originalState`/`currentState`)
    */
-  changesSummary?: ChangeSummary;
+  changeSummary?: ChangeSummary;
 }
 
-export const KAFKA_TOPIC_NAME = 'activity_log_created';
+/** Use for the kafka topic name */
+export const ACTIVITY_LOG_EVENT = 'activity_log_created';
