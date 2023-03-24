a69ca88b5b8cf06d8353fb343d13985fea586543
Quốc Hưng <quochung@evol.vn>
2023-03-24 17:45:19 +0700

    BEIN-13410 Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports', 'import'],
   extends: [
