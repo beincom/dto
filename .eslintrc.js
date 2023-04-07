ef3be704a23d4f5f7eb50f47059dff73e2c037a2
Quốc Hưng <quochung@evol.vn>
2023-04-07 23:44:18 +0700

    BEIN-13411 Update file eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: './tsconfig.json',
     sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports', 'import'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'prettier',
@@ -18,14 +18,27 @@ module.exports = {
   },
   ignorePatterns: ['dist', '.eslintrc.js', 'webpack.config.js', 'layers'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    'no-console': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
     'prettier/prettier': [
       2,
       {
@@ -33,6 +46,44 @@ module.exports = {
       },
     ],
     curly: 'error',
-    'import/no-duplicates': 'error',
+
+    // Naming convention
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'method', // memberLike
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
+    ],
+
+    // Await rules
+    'require-await': 'off',
+    '@typescript-eslint/require-await': ['error'],
+    '@typescript-eslint/return-await': ['error', 'in-try-catch'],
+
+    // Explicit accessibility
+    '@typescript-eslint/explicit-member-accessibility': 'off',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          {
+            accessibility: 'explicit',
+            overrides: {
+              accessors: 'explicit',
+              constructors: 'no-public',
+              methods: 'explicit',
+              properties: 'no-public',
+              parameterProperties: 'explicit',
+            },
+          },
+        ],
+      },
+    },
+  ],
 };
