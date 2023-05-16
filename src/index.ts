e5f6b4e2274d921e07ebeb8e1c0a43374d0fe7ee
Hoang Minh TRong <minhtrong@evol.vn>
2023-05-16 11:22:56 +0700

    share UserId value object

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './user.dto';
 export * from './activity-log.dto';
 export * from './activity-log-usecase';
+export * from './user-id.value-object';
---
+++ src/user-id.value-object.ts
@@ -0,0 +1,11 @@
+import { UUID, ValueObjectProperties } from '@beincom/domain';
+
+export class UserId extends UUID {
+    constructor(props: ValueObjectProperties<string>) {
+        super(props);
+    }
+
+    public static fromString(value: string) {
+        return new UserId({ value });
+    }
+}
