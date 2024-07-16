16a8ee9d5dc81669352fe615370d116ceb488bdb
Trang Ha <diemtrang@evol.vn>
2024-07-16 10:15:07 +0700

    feat(patch): BEIN-26635 add SharedGroupDto

---
+++ src/group.dto.ts
@@ -0,0 +1,21 @@
+import { GROUP_PRIVACY } from '@beincom/constants';
+
+export type InnerGroupIdsByPrivacy = {
+  open: string[];
+  closed: string[];
+  private: string[];
+  secret: string[];
+};
+
+export interface SharedGroupDto {
+  id: string;
+  communityId?: string;
+  name: string;
+  icon: string;
+  privacy: GROUP_PRIVACY;
+  parentIds: string[];
+  child?: InnerGroupIdsByPrivacy;
+  isCommunity?: boolean;
+  rootGroupId: string;
+  isLinkedNft: boolean;
+}
