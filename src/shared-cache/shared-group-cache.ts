3d5082657527f5b55684133a458a85d3c14572dc
Dat Nguyen Minh <minhdat@evol.vn>
2024-09-11 16:52:02 +0700

    feat: update hash tag for share db

--- src/shared-cache/shared-group-cache.ts
+++ src/shared-cache/shared-group-cache.ts
@@ -4,22 +4,22 @@ export class SharedGroupCacheKey {
   private static PREFIX = 'group';
 
   public static groupById(groupId: string): string {
-    return [SharedGroupCacheKey.PREFIX, 'shared_group', groupId].join(':');
+    return [SharedGroupCacheKey.PREFIX, `{1}`, 'shared_group', groupId].join(':');
   }
 
   public static joinedGroupsOfUser(userId: string): string {
-    return [SharedGroupCacheKey.PREFIX, CACHE_KEYS.JOINED_GROUPS, userId].join(':');
+    return [SharedGroupCacheKey.PREFIX, `{1}`, CACHE_KEYS.JOINED_GROUPS, userId].join(':');
   }
 
   public static communityPermissionsOfUser(userId: string): string {
-    return [SharedGroupCacheKey.PREFIX, CACHE_KEYS.COMMUNITY_PERMISSION, userId].join(':');
+    return [SharedGroupCacheKey.PREFIX, `{1}`, CACHE_KEYS.COMMUNITY_PERMISSION, userId].join(':');
   }
 
   public static groupPermissionsOfUser(userId: string): string {
-    return [SharedGroupCacheKey.PREFIX, CACHE_KEYS.GROUP_PERMISSION, userId].join(':');
+    return [SharedGroupCacheKey.PREFIX, `{1}`, CACHE_KEYS.GROUP_PERMISSION, userId].join(':');
   }
 
   public static showingBadgeOfUser(userId: string): string {
-    return [SharedGroupCacheKey.PREFIX, CACHE_KEYS.SHOWING_BADGES, userId].join(':');
+    return [SharedGroupCacheKey.PREFIX, `{1}`, CACHE_KEYS.SHOWING_BADGES, userId].join(':');
   }
 }
