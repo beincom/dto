07b25d6eaa4d4b361cdd330361d6d6e09d69f201
Trang Ha <diemtrang@evol.vn>
2024-12-27 08:06:46 +0700

    fix(patch): undefined this when using point-free style

--- src/shared-cache/shared-mission-cache.ts
+++ src/shared-cache/shared-mission-cache.ts
@@ -5,34 +5,58 @@ export class SharedMissionCacheKey {
   private static hashTag = '{1}';
 
   public static getJackpotAchieved(): string {
-    return [this.PREFIX, this.hashTag, CACHE_KEYS.MISSION.JACKPOT_ACHIEVED].join(':');
+    return [
+      SharedMissionCacheKey.PREFIX,
+      SharedMissionCacheKey.hashTag,
+      CACHE_KEYS.MISSION.JACKPOT_ACHIEVED,
+    ].join(':');
   }
 
   public static getJackpotMedalConversionRate(): string {
     return [
-      this.PREFIX,
-      this.hashTag,
+      SharedMissionCacheKey.PREFIX,
+      SharedMissionCacheKey.hashTag,
       CACHE_KEYS.MISSION.JACKPOT_MEDAL_CONVERSION_RATE,
     ].join(':');
   }
 
   public static getJackpotSegments(): string {
-    return [this.PREFIX, this.hashTag, CACHE_KEYS.MISSION.JACKPOT_SEGMENTS].join(':');
+    return [
+      SharedMissionCacheKey.PREFIX,
+      SharedMissionCacheKey.hashTag,
+      CACHE_KEYS.MISSION.JACKPOT_SEGMENTS,
+    ].join(':');
   }
 
   public static getJackpotActivities(): string {
-    return [this.PREFIX, this.hashTag, CACHE_KEYS.MISSION.JACKPOT_ACTIVITIES].join(':');
+    return [
+      SharedMissionCacheKey.PREFIX,
+      SharedMissionCacheKey.hashTag,
+      CACHE_KEYS.MISSION.JACKPOT_ACTIVITIES,
+    ].join(':');
   }
 
   public static getRateLimiter(): string {
-    return [this.PREFIX, this.hashTag, CACHE_KEYS.MISSION.RATE_LIMIT].join(':');
+    return [
+      SharedMissionCacheKey.PREFIX,
+      SharedMissionCacheKey.hashTag,
+      CACHE_KEYS.MISSION.RATE_LIMIT,
+    ].join(':');
   }
 
   public static getJackpotNRU(): string {
-    return [this.PREFIX, this.hashTag, CACHE_KEYS.MISSION.JACKPOT_NRU].join(':');
+    return [
+      SharedMissionCacheKey.PREFIX,
+      SharedMissionCacheKey.hashTag,
+      CACHE_KEYS.MISSION.JACKPOT_NRU,
+    ].join(':');
   }
 
   public static getJackpotRef(): string {
-    return [this.PREFIX, this.hashTag, CACHE_KEYS.MISSION.JACKPOT_REF].join(':');
+    return [
+      SharedMissionCacheKey.PREFIX,
+      SharedMissionCacheKey.hashTag,
+      CACHE_KEYS.MISSION.JACKPOT_REF,
+    ].join(':');
   }
 }
